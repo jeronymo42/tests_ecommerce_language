f455ec1c8d010d9cf4fb10299311eee411328f90
V.Ledovskikh <vladimir.ledovskikh@gmail.com>
2023-10-06 01:11:05 +0300

    change function name

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_step(browser):
+def test_find_card_button(browser):
     browser.get(link)
     add_button = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
     assert len(add_button) > 0, 'Элемент не найден!'
